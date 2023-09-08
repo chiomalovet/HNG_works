4bdb1daa54f8d91252de9304702e5588ed7b05c8
chiomalovet <110299085+chiomalovet@users.noreply.github.com>
2023-09-08 18:25:45 +0100

    HNG task 1

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function updateDayOfWeek() {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const now = new Date();
+    const dayOfWeekString = daysOfWeek[now.getDay()];
+    document.getElementById('dayOfWeek').textContent = dayOfWeekString;
+}
+
+updateDayOfWeek(); // Initial update
+setInterval(updateDayOfWeek, 1000 * 60 * 60 * 24);
+
+function displayCurrentTime() {
+    var currentTimeMilliseconds = new Date().getTime();
+    document.getElementById("time").textContent = currentTimeMilliseconds;
+}
+
+
+displayCurrentTime();
+
+
+setInterval(displayCurrentTime, 100);
